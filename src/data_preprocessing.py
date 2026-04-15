d2e0d6d56f93c22e29ad001b245b1d5aa65a34c6
aayu04shi <aayushi636naik@gmail.com>
2026-04-16 02:05:59 +0530

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,6 +4,7 @@ def load_data(path):
     return pd.read_csv(path)
 
 def clean_data(df):
+    df.columns = df.columns.str.strip()
     df = df.drop_duplicates()
     df = df.fillna({
         "Victim Age": df["Victim Age"].median(),
